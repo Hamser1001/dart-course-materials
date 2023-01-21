418affbfff7f8120e9391e2ddea670faf90f2b9e
Hamza Serhani <upscale.mind50@gmail.com>
2023-01-21 22:10:35 +0100

    Update 02.17-temperature-conversion.dart

--- solutions/02.17-temperature-conversion.dart
+++ solutions/02.17-temperature-conversion.dart
@@ -2,7 +2,7 @@ void main() {
   double tempFarenheit = 90.25;
   double tempCelsius = (tempFarenheit - 32) / 1.8;
   print(
-      '${tempFarenheit.toStringAsFixed(1)}F = ${tempCelsius.toStringAsFixed(1)}C');
+      '${tempFarenheit.toStringAsFixed(0)}F = ${tempCelsius.toStringAsFixed(0)}C');
 
   // Fahrenheit to Celsius: (°F − 32) / 1.8 = °C
   // Desired output:
